56fd8132f1c8443392026f3851e22a4b6b0f8de0
Steven Dickerson <poddocdan@gmail.com>
2024-04-03 08:00:43 -0700

    changed i to iterator

--- app.js
+++ app.js
@@ -15,38 +15,39 @@ console.log("EXERCISE 2:\n==========\n");
 
 for (let i = 1; i <= 100; i++) {
   if (i % 3 === 0 && i % 5 === 0) {
-    console.log("FIZZBUZZ");
-  } else if (i % 3 === 0) {
-    console.log("FIZZ");
-  } else if (i % 5 === 0) {
-    console.log("BUZZ");
-  }
-}
-
-// Exercise 3
-console.log("EXERCISE 3:\n==========\n");
-// DO exercise 1
-let i = 0;
-while (i <= 100) {
-  if (i % 2 !== 0) {
-    console.log(i);
-  } else {
-    continue;
-  }
-}
-// DO WHILE of exercise 2
-do {
-  if (i % 3 === 0 && i % 5 === 0) {
     console.log("FIZZBUZZ", i);
   } else if (i % 3 === 0) {
     console.log("FIZZ", i);
   } else if (i % 5 === 0) {
     console.log("BUZZ", i);
-  } else {
-    console.log(i);
   }
-  i++;
-} while (i <= 100);
+}
+
+// Exercise 3
+console.log("EXERCISE 3:\n==========\n");
+let iterator = 1;
+while (iterator <= 100) {
+  if (iterator % 3 === 0 && iterator % 5 === 0) {
+    console.log("FIZZBUZZ", iterator);
+  } else if (iterator % 3 === 0) {
+    console.log("FIZZ", iterator);
+  } else if (iterator % 5 === 0) {
+    console.log("BUZZ", iterator);
+  }
+  iterator++;
+}
+
+iterator = 1;
+do {
+  if (iterator % 3 === 0 && iterator % 5 === 0) {
+    console.log("FIZZBUZZ", iterator);
+  } else if (iterator % 3 === 0) {
+    console.log("FIZZ", iterator);
+  } else if (iterator % 5 === 0) {
+    console.log("BUZZ", iterator);
+  }
+  iterator++;
+} while (iterator <= 100);
 
 // Exercise 4
 let value = Math.round(Math.random() * 500); // creates a random number between 0 and 500
@@ -57,7 +58,7 @@ for (let i = 0; i <= n; i++) {
   if (i === value) {
     console.log("Found value!");
     //   If found within the loop, it prints "Found value!".
-    break; // exits the loop
+    break; // exits the loop because we found what we wanted
   } else if (i === n) {
     console.log("Did not find value");
     //   If the value is not found after going through all numbers, it prints "Did not find value".
